ca7d8ef0bd584b5e468f8ca4bfcf79eac4445f26
Soarnir <soarnir@gmail.com>
2022-05-12 22:44:18 +0200

    attempt force checkstyle

--- src/main/java/MQTT_Publisher.java
+++ src/main/java/MQTT_Publisher.java
@@ -8,15 +8,14 @@ public class MQTT_Publisher {
 
     public static void main(String[] args) {
 
-        String topic        = "/smartcar/control/throttle";
+        String tOpIC        = "/smartcar/control/throttle";
         String content      = "0";
         int qos             = 2;
         String broker       = "tcp://localhost:1883";
         String clientId     = "MQTT-publisher";
         MemoryPersistence persistence = new MemoryPersistence();
 
-        try {
-            //Generate MQTT client
+        try{//Generate MQTT client
             MqttClient sampleClient = new MqttClient(broker, clientId, persistence);
             MqttConnectOptions connOpts = new MqttConnectOptions();
             connOpts.setCleanSession(true);
@@ -30,7 +29,7 @@ public class MQTT_Publisher {
             System.out.println("Publishing message: " + content);
             MqttMessage message = new MqttMessage(content.getBytes());
             message.setQos(qos);
-            sampleClient.publish(topic, message);
+            sampleClient.publish(tOpIC, message);
             System.out.println("Message published");
 
             //Disconnect client
@@ -39,14 +38,14 @@ public class MQTT_Publisher {
 
             //Exit program
             System.exit(0);
-        } catch (MqttException me) {
+        }catch (MqttException me){
             //Standard error printing
-            System.out.println("reason " + me.getReasonCode());
-            System.out.println("msg " + me.getMessage());
-            System.out.println("loc " + me.getLocalizedMessage());
+        System.out.println("reason " + me.getReasonCode());
+         System.out.println("msg " + me.getMessage());
+           System.out.println("loc " + me.getLocalizedMessage());
             System.out.println("cause " + me.getCause());
-            System.out.println("excep " + me);
-            me.printStackTrace();
+             System.out.println("excep " + me);
+              me.printStackTrace();
         }
     }
 }
