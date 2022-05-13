52285797a878cc9155ba9cb3197a90f65c248f68
Soarnir <soarnir@gmail.com>
2022-05-14 00:22:23 +0200

    Alter checkstyle, multiple fixes

--- src/main/java/MQTTPublisher.java
+++ src/main/java/MQTTPublisher.java
@@ -4,18 +4,19 @@ import org.eclipse.paho.client.mqttv3.MqttException;
 import org.eclipse.paho.client.mqttv3.MqttMessage;
 import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence;
 
-public class MQTT_Publisher {
+public class MQTTPublisher {
 
     public static void main(String[] args) {
 
-        String tOpIC        = "/smartcar/control/throttle";
+        String topic        = "/smartcar/control/throttle";
         String content      = "0";
         int qos             = 2;
         String broker       = "tcp://localhost:1883";
         String clientId     = "MQTT-publisher";
         MemoryPersistence persistence = new MemoryPersistence();
 
-        try {//Generate MQTT client
+        try {
+            //Generate MQTT client
             MqttClient sampleClient = new MqttClient(broker, clientId, persistence);
             MqttConnectOptions connOpts = new MqttConnectOptions();
             connOpts.setCleanSession(true);
@@ -29,7 +30,7 @@ public class MQTT_Publisher {
             System.out.println("Publishing message: " + content);
             MqttMessage message = new MqttMessage(content.getBytes());
             message.setQos(qos);
-            sampleClient.publish(tOpIC, message);
+            sampleClient.publish(topic, message);
             System.out.println("Message published");
 
             //Disconnect client
@@ -44,8 +45,8 @@ public class MQTT_Publisher {
             System.out.println("msg " + me.getMessage());
             System.out.println("loc " + me.getLocalizedMessage());
             System.out.println("cause " + me.getCause());
-             System.out.println("excep " + me);
-              me.printStackTrace();
+            System.out.println("excep " + me);
+            me.printStackTrace();
         }
     }
 }
